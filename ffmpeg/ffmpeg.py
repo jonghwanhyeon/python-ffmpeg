9227932d65c4c824438df574c9c390dc0da01213
Jonghwan Hyeon <hyeon0145@gmail.com>
2018-06-04 10:35:58 +0900

    Encapsulate instance variables

--- ffmpeg/ffmpeg.py
+++ ffmpeg/ffmpeg.py
@@ -11,72 +11,76 @@ class FFmpegError(Exception):
 
 
 class FFmpeg(EventEmitter):
-    File = namedtuple('File', ['url', 'options'])
+    _File = namedtuple('_File', ['url', 'options'])
 
     def __init__(self, executable='ffmpeg'):
         super().__init__()
 
-        self.executable = executable
-        self.global_options = {}
-        self.input_files = []
-        self.output_files = []
+        self._executable = executable
+        self._global_options = {}
+        self._input_files = []
+        self._output_files = []
 
-        self.executed = False
-        self.terminated = False
-        self.process = None
+        self._executed = False
+        self._terminated = False
+        self._process = None
 
         self.on('stderr', self._on_stderr)
 
     def option(self, key, value=None):
-        self.global_options[key] = value
+        self._global_options[key] = value
         return self
 
     def input(self, url, options=None, **kwargs):
         if options is None:
             options = {}
 
-        self.input_files.append(FFmpeg.File(url=url, options={**options, **kwargs}))
+        self._input_files.append(FFmpeg._File(url=url, options={**options, **kwargs}))
         return self
 
     def output(self, url, options=None, **kwargs):
         if options is None:
             options = {}
 
-        self.output_files.append(FFmpeg.File(url=url, options={**options, **kwargs}))
+        self._output_files.append(FFmpeg._File(url=url, options={**options, **kwargs}))
         return self
 
+    @property
+    def process(self):
+        return self._process
+
     async def execute(self):
-        if self.executed:
+        if self._executed:
             raise FFmpegError('FFmpeg is already executed')
 
         arguments = self._build()
         self.emit('start', arguments)
 
-        self.process = await asyncio.create_subprocess_exec(
+        self._process = await asyncio.create_subprocess_exec(
             *arguments,
             stderr=asyncio.subprocess.PIPE
         )
 
-        self.executed = True
+        self._executed = True
         await self._read_stderr()
-        await self.process.wait()
+        await self._process.wait()
 
-        if self.process.returncode == 0:
+        if self._process.returncode == 0:
             self.emit('completed')
-        elif self.terminated:
+        elif self._terminated:
             self.emit('terminated')
         else:
-            self.emit('error', self.process.returncode)
+            self.emit('error', self._process.returncode)
 
     def terminate(self):
-        if not self.executed:
+        if not self._executed:
             raise FFmpegError('FFmpeg is not executed')
 
-        self.terminated = True
-        self.process.terminate()
+        self._terminated = True
+        self._process.terminate()
 
     async def _read_stderr(self):
-        async for line in readline(self.process.stderr):
+        async for line in readline(self._process.stderr):
             self.emit('stderr', line.decode('utf-8'))
 
     def _on_stderr(self, line): # registered in __init__()
@@ -85,14 +89,14 @@ class FFmpeg(EventEmitter):
             self.emit('progress', progress)
 
     def _build(self):
-        arguments = [self.executable]
-        arguments.extend(build_options(self.global_options))
+        arguments = [self._executable]
+        arguments.extend(build_options(self._global_options))
 
-        for file in self.input_files:
+        for file in self._input_files:
             arguments.extend(build_options(file.options))
             arguments.extend(['-i', file.url])
 
-        for file in self.output_files:
+        for file in self._output_files:
             arguments.extend(build_options(file.options))
             arguments.append(file.url)
 
