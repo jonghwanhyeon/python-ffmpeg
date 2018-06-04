460d08e8c4d897192db23d12f3113a533ff55802
Jonghwan Hyeon <hyeon0145@gmail.com>
2018-06-04 20:32:27 +0900

    Remove Python 3.5

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only'
     ],
