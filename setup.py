13d1ab627feeca1ec51ce2c499694107d7292c3c
Jonghwan Hyeon <jonghwanhyeon@kaist.ac.kr>
2019-02-04 20:12:44 +0900

    Version 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-ffmpeg',
-    version='1.0.5',
+    version='1.0.6',
     description='A python interface for FFmpeg',
     url='https://github.com/jonghwanhyeon/python-ffmpeg',
     author='Jonghwan Hyeon',
