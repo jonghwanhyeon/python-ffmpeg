1c25b1d4dbee52eb1b9d889c4301539d4ef38bf2
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-01-28 15:56:26 +0900

    Add example for demonstrating multiple-value options

---
+++ examples/video-from-a-audio-from-b.py
@@ -0,0 +1,43 @@
+import asyncio
+from ffmpeg import FFmpeg
+
+
+ffmpeg = (FFmpeg()
+          .option('y')
+          .input('input-a.mp4')
+          .input('input-b.mp4')
+          .output('output.mp4',
+                  map=['0:0', '1:1']))
+
+
+@ffmpeg.on('start')
+def on_start(arguments):
+    print('arguments:', arguments)
+
+
+@ffmpeg.on('stderr')
+def on_stderr(line):
+    print('stderr:', line)
+
+
+@ffmpeg.on('progress')
+def on_progress(progress):
+    print(progress)
+
+
+@ffmpeg.on('completed')
+def on_completed():
+    print('completed')
+
+
+@ffmpeg.on('terminated')
+def on_terminated():
+    print('terminated')
+
+
+@ffmpeg.on('error')
+def on_error(code):
+    print('error:', code)
+
+
+asyncio.run(ffmpeg.execute())
